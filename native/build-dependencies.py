661a6e9024e09a9dd7d8f25c8559a13c39421f46
Matt Cooley <macool@microsoft.com>
2024-01-10 07:27:06 -0800

    Fixes for Windows build

--- native/build-dependencies.py
+++ native/build-dependencies.py
@@ -1,17 +1,23 @@
-# Required to build on Ubuntu 22.04:
+# On Ubuntu 22.04, make sure the following are installed:
 # cmake
 # clang
 # libc++-14-dev
 # libc++abi-14-dev
 # ninja-build
 
+# On Windows, run from "x64 Native Tools Command Prompt for VS 2022"
+
 import os
 import subprocess
 
 print('Building SwiftShader...')
-swiftshader_build_dir = os.path.dirname(os.path.realpath(__file__)) + '/swiftshader/build'
+swiftshader_dir = os.path.dirname(os.path.realpath(__file__)) + '/swiftshader'
+os.chdir(swiftshader_dir)
+subprocess.run(['git', 'apply', '../swiftshader_no_download_commit_hook.patch'])
+swiftshader_build_dir = swiftshader_dir + '/out'
+if not os.path.exists(swiftshader_build_dir):
+    os.makedirs(swiftshader_build_dir)
 os.chdir(swiftshader_build_dir)
-subprocess.run(['git', 'apply', '../../swiftshader_no_download_commit_hook.patch'])
 subprocess.run(['cmake', '..',
     '-GNinja',
     '-DCMAKE_BUILD_TYPE=Release',
@@ -23,21 +29,24 @@ subprocess.run(['ninja'])
 os.environ["SWIFTSHADER_LD_LIBRARY_PATH"] = swiftshader_build_dir
 
 print('Building Filament...')
-filament_build_dir = os.path.dirname(os.path.realpath(__file__)) + '/filament/out'
+filament_dir = os.path.dirname(os.path.realpath(__file__)) + '/filament'
+os.chdir(filament_dir)
+subprocess.run(['git', 'apply', '../filament_use_swiftshader_relative_path.patch'])
+filament_build_dir = filament_dir + '/out'
 if not os.path.exists(filament_build_dir):
-    os.mkdirs(filament_build_dir)
+    os.makedirs(filament_build_dir)
 os.chdir(filament_build_dir)
-subprocess.run(['git', 'apply', '../../filament_use_swiftshader_relative_path.patch'])
 subprocess.run(['cmake', '..',
     '-GNinja',
     '-DCMAKE_BUILD_TYPE=Release',
+    '-DFILAMENT_SUPPORTS_VULKAN=ON',
     '-DFILAMENT_SUPPORTS_OPENGL=OFF',
     '-DFILAMENT_SUPPORTS_METAL=OFF',
     '-DFILAMENT_SUPPORTS_XCB=OFF',
     '-DFILAMENT_SUPPORTS_XLIB=OFF',
     '-DFILAMENT_SKIP_SAMPLES=ON',
     '-DFILAMENT_SKIP_SDL2=ON',
-    '-DFILAMENT_USE_SWIFTSHADER=ON'])
+    '-DUSE_STATIC_CRT=OFF'])
 subprocess.run(['ninja'])
 
 filament_sdk_dir = filament_build_dir + '/sdk'
